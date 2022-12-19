d034c9fa2852cbbfe5feb93852891d19d97c017c
Marina <marina.gazogareeva@gmail.com>
2022-12-19 14:57:53 +0300

    added task66

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+void rec(string s2, string c)
+{
+    string s1;
+    int i;
+    if (s2.Length == 0)
+        Console.WriteLine(c);
+    else
+    {
+        for (i = 0; i < s2.Length; i++)
+        {
+            c += s2[i];
+            s1 = s2;
+            s1 = s1.Remove(i, 1);
+            rec(s1, c);
+            c = c.Remove(c.Length - 1, 1);
+        }
+         
+         }
+void rec(int i, int s1, int summ)
+{
+  summ = summ + s2;
+  if (s2 <= s1)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  Console.WriteLine(s1, s2 - 1, summ);
+}
+
+}
+Console.Clear();
+string? s = Console.ReadLine(), c, s2;
+for(int i = 0; i < s.Length; i++)
+{
+    c = s[i].ToString();
+    s2 = s;
+    s2 = s2.Remove(i, 1);
+    //Console.WriteLine(s2);
+    rec(s2, c);
+}
+
