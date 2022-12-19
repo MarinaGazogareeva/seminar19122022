6e8f3bc3c2d1fecc3c8f89f09938619567dfd22f
Marina Gazogareeva <118352996+MarinaGazogareeva@users.noreply.github.com>
2022-12-19 18:04:27 +0300

    Update Program.cs
    added task 66 and 68

--- Program.cs
+++ Program.cs
@@ -1,41 +1,50 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-void rec(string s2, string c)
-{
-    string s1;
-    int i;
-    if (s2.Length == 0)
-        Console.WriteLine(c);
-    else
-    {
-        for (i = 0; i < s2.Length; i++)
-        {
-            c += s2[i];
-            s1 = s2;
-            s1 = s1.Remove(i, 1);
-            rec(s1, c);
-            c = c.Remove(c.Length - 1, 1);
-        }
-         
-         }
-void rec(int i, int s1, int summ)
-{
-  summ = summ + s2;
-  if (s2 <= s1)
-  {
-    Console.Write($"Сумма элементов= {summ} ");
-    return;
-  }
-  Console.WriteLine(s1, s2 - 1, summ);
-}
 
+int rec(int n, int m); 
+
+{
+   if (m == 0) 
+   return (n * (n + 1)) / 2;            
+   else if (n == 0)
+    return (m * (m + 1)) / 2;       
+    else if (m == n) 
+    return M;                       
+    else if (m < n) 
+    return n + rec(m, n - 1); 
+    else 
+    return n + rec(m, n + 1);    
 }
 Console.Clear();
-string? s = Console.ReadLine(), c, s2;
-for(int i = 0; i < s.Length; i++)
+Console.Write("Введите 1-е число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
 {
-    c = s[i].ToString();
-    s2 = s;
-    s2 = s2.Remove(i, 1);
-    rec(s2, c);
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
 }
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+
 
